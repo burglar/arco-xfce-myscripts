4033fa27f2658878d67673990a2f25dd6e706df2
Emanuel Magalhaes <emanueldacruz@gmail.com>
2020-11-27 12:28:27 +0000

    Add personal alias to script 400

--- 400-extra-funcionality-bash.sh
+++ 400-extra-funcionality-bash.sh
@@ -1,12 +1,27 @@
+echo "
+
+#####################
+### My alias
+#####################
+
+alias clear="clear;neofetch uptime local_ip packages disk memory"
+alias e=subl3
+alias rc="source ~/.bashrc-personal"
+
+" >> ~/.bashrc-personal
 git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt --depth=1
 echo "
 
+# Load git prompt
+# https://github.com/magicmonty/bash-git-prompt
 if [ -f "$HOME/.bash-git-prompt/gitprompt.sh" ]; then
     GIT_PROMPT_ONLY_IN_REPO=1
     source $HOME/.bash-git-prompt/gitprompt.sh
 fi
 
+# Theme for git prompt
 export GIT_PROMPT_THEME=Single_line
 
 " >> ~/.bashrc-personal
 . ~/.bashrc-personal
+
