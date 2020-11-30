8abcfd3da9cf65007d06f2c0081225b30d726e1f
Emanuel Magalhaes <emanueldacruz@gmail.com>
2020-11-30 18:26:02 +0000

    Add check for existing files

--- zsh/300-update-dot-files.sh
+++ zsh/300-update-dot-files.sh
@@ -1,3 +1,24 @@
+#!/bin/bash
+#
+
+now=$(date +"%Y-%m-%d_%HH%M")
+
+
+if [[ -f ~/.zshrc ]]; then
+    cp ~/.zshrc ~/.zshrc_$now
+fi
 cp .zshrc ~/.zshrc
+
+
+
+if [[ -f ~/.zshrc ]]; then
+    cp ~/.p10k.zsh ~/.p10k.zsh_$now
+fi
 cp .p10k.zsh ~/.p10k.zsh
+
+
+
+if [[ -f ~/.zshrc ]]; then
+    cp ~/.config/aliasrc ~/.config/aliasrc_$now
+fi
 cp aliasrc ~/.config/aliasrc
