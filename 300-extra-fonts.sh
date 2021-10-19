2431782ce4cb9f5573b6fdb173ef93d48d0e8950
Emanuel Magalhaes <emanueldacruz@gmail.com>
2021-10-20 00:20:09 +0100

    Update 300-extra-fonts.sh

--- 300-extra-fonts.sh
+++ 300-extra-fonts.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 #
+
+
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/JetBrainsMono.zip -P ~/Downloads
+mkdir -p ~/.fonts
+unzip ~/Downloads/JetBrainsMono.zip -d ~/.fonts
+fc-cache -v
