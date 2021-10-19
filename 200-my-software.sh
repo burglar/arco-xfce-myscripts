5fd1c5ff0192c22a1f5cfbb50386933e9342418d
Emanuel Magalhaes <emanueldacruz@gmail.com>
2021-10-19 23:43:05 +0100

    Update 200-my-software.sh

--- 200-my-software.sh
+++ 200-my-software.sh
@@ -1,2 +1,51 @@
 #!/bin/bash
-#
+#set -e
+###############################################################################
+# Based on the scripts from arco linux by Erik Dubois (https://www.arcolinux.com)
+###############################################################################
+
+
+func_install() {
+	if pacman -Qi $1 &> /dev/null; then
+		tput setaf 2
+  		echo "###############################################################################"
+  		echo "################## The package "$1" is already installed"
+      	echo "###############################################################################"
+      	echo
+		tput sgr0
+	else
+    	tput setaf 3
+    	echo "###############################################################################"
+    	echo "##################  Installing package "  $1
+    	echo "###############################################################################"
+    	echo
+    	tput sgr0
+    	sudo pacman -S --noconfirm --needed $1 
+    fi
+}
+
+###############################################################################
+echo "Installation of software"
+###############################################################################
+
+list=(
+sublime-text-4
+librewolf-bin
+gitahead-bin
+)
+
+count=0
+
+for name in "${list[@]}" ; do
+	count=$[count+1]
+	tput setaf 3;echo "Installing package nr.  "$count " " $name;tput sgr0;
+	func_install $name
+done
+
+###############################################################################
+
+tput setaf 11;
+echo "################################################################"
+echo "Software has been installed"
+echo "################################################################"
+echo;tput sgr0
